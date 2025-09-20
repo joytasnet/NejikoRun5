ca8479f63dbc8442ac5ee821c150243c847f1646
mjpurin <mjpurin@gmail.com>
2025-09-20 20:04:52 +0900

    P271

---
+++ Assets/Scripts/StageGenerator.cs
@@ -0,0 +1,71 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StageGenerator : MonoBehaviour
+{
+    const int StageChipSize = 30;
+    //生成済の最大Index
+    int currentChipIndex;
+
+    public Transform character;
+    public GameObject[] stageChips;
+    public int startChipIndex;
+    public int preInstantiate;
+    public List<GameObject> generatedStageList = new List<GameObject>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentChipIndex = startChipIndex - 1;
+        UpdateStage(preInstantiate);
+        //Debug.Break();
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int charaPositionIndex = (int)(character.position.z / StageChipSize);
+        if (charaPositionIndex + preInstantiate > currentChipIndex)
+        {
+            UpdateStage(charaPositionIndex + preInstantiate);
+        }
+        
+    }
+    void UpdateStage(int toChipIndex)
+    {
+        if (toChipIndex <= currentChipIndex) return;
+
+        for (int i = currentChipIndex + 1; i <= toChipIndex; i++)
+        {
+            GameObject stageObject = GenerateStage(i);
+            generatedStageList.Add(stageObject);
+        }
+
+        while (generatedStageList.Count > preInstantiate + 2)
+        {
+            DestroyOldestStage();
+        }
+        currentChipIndex = toChipIndex;
+
+    }
+    GameObject GenerateStage(int chipIndex)
+    {
+        int nextStageChip = Random.Range(0, stageChips.Length);
+
+        GameObject stageObject = Instantiate(
+            stageChips[nextStageChip],
+            new Vector3(0,0,chipIndex * StageChipSize),
+            Quaternion.identity
+        );
+        return stageObject;
+
+    }
+    void DestroyOldestStage()
+    {
+        GameObject oldStage = generatedStageList[0];
+        generatedStageList.RemoveAt(0);
+        Destroy(oldStage);
+
+    }
+}
